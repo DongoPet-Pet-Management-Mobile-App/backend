b235860a3534ac69b236a52d99a811da344f503d
denysbakin <denysbakin@gmail.com>
2025-07-28 14:02:20 -0400

    add upload dir as mount

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ import sentry_sdk
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
 
 from app.api.main import api_router
 from app.core.config import settings
@@ -19,6 +20,8 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
 )
+
+app.mount("/upload", StaticFiles(directory="/var/lib/dongopet/backend/backend/upload"), name="upload")
 
 # Set all CORS enabled origins
 if settings.all_cors_origins:
